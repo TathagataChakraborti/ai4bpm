09f8e6ba4ae05b741afc7ce64a3d35f2b190a4b9
Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
2023-10-01 22:02:56 -0400

    Updates

---
+++ precache-manifest.08d19909c8f4f6848b605c125f7bea0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c76280645b5454e78536",
+    "url": "/static/css/main.53e1650b.chunk.css"
+  },
+  {
+    "revision": "c76280645b5454e78536",
+    "url": "/static/js/main.81117013.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "031b4265a6c7d5afece9",
+    "url": "/static/js/2.363f9179.chunk.js"
+  },
+  {
+    "revision": "3fc379d3bf807c17fc231f99bb549a1c",
+    "url": "/index.html"
+  }
+];
